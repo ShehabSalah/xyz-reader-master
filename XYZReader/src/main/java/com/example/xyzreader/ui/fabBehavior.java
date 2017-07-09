4be44b3be58ac46ced3ee4ea5e434d6ef0acdf3d
Shehab Salah <shehabsalah25@gmail.com>
2017-07-09 10:37:19 +0200

    Initial commit

---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/fabBehavior.java
@@ -0,0 +1,57 @@
+package com.example.xyzreader.ui;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v13.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Shehab Salah on 2017-07-09.
+ */
+
+public class fabBehavior extends FloatingActionButton.Behavior {
+
+    public fabBehavior(Context context, AttributeSet attrs) {
+        super();
+    }
+
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout,
+        FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
+        // Ensure we react to vertical scrolling
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL
+        || super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, nestedScrollAxes);
+
+    }
+
+    @Override
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child,
+        View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+        if (dyConsumed > 0) {
+        // User scrolled up -> hide the FAB
+            child.hide(new FloatingActionButton.OnVisibilityChangedListener() {
+                /**
+                 * Called when a FloatingActionButton has been hidden
+                 *
+                 * @param fab the FloatingActionButton that was hidden.
+                 */
+                @Override
+                public void onHidden(FloatingActionButton fab) {
+                    super.onShown(fab);
+                    fab.setVisibility(View.INVISIBLE);
+                }
+            });
+        } else if (dyConsumed < 0) {
+            child.show(new FloatingActionButton.OnVisibilityChangedListener() {
+                @Override
+                public void onShown(FloatingActionButton fab) {
+                    super.onShown(fab);
+                    fab.setVisibility(View.VISIBLE);
+                }
+            });
+        }
+    }
+}
